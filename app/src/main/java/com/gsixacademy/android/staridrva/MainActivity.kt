4c49fb861e19b58e314dbfc78bf03ba30002c48f
Sajdini <ajdinis1993@gmail.com>
2020-08-13 19:10:47 +0200

    fragments created

--- app/src/main/java/com/gsixacademy/android/staridrva/MainActivity.kt
+++ app/src/main/java/com/gsixacademy/android/staridrva/MainActivity.kt
@@ -2,10 +2,32 @@ package com.gsixacademy.android.staridrva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.bottom_navigation_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController:NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    navController=Navigation.findNavController(this,R.id.navigation_host_fragment)
+        initListeners()
+    }
+    fun initListeners(){
+        text_view_home.setOnClickListener {
+            navController.navigate(R.id.action_global_homeFragment)
+        }
+        text_view_map.setOnClickListener {
+
+        }
+        text_view_list.setOnClickListener {
+
+        }
+        text_view_info.setOnClickListener {
+
+        }
     }
 }
--- app/src/main/java/com/gsixacademy/android/staridrva/views/CustomBottomNavigation.kt
+++ app/src/main/java/com/gsixacademy/android/staridrva/views/CustomBottomNavigation.kt
@@ -1,4 +1,31 @@
 package com.gsixacademy.android.staridrva.views
 
-class CustomBottomNavigation {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.gsixacademy.android.staridrva.R
+import java.util.jar.Attributes
+import java.util.zip.Inflater
+
+class CustomBottomNavigation:FrameLayout {
+
+    lateinit var mInflater: LayoutInflater
+
+    constructor(context:Context):super(context){
+        init(context)
+    }
+    constructor(context: Context,attr:AttributeSet?):super(context,attr){
+        init(context)
+    }
+    constructor(context: Context,attr:AttributeSet?,style:Int):super(context,attr,style){
+        init(context)
+    }
+
+
+    fun init(context: Context){
+        mInflater= LayoutInflater.from(context)
+        mInflater.inflate(R.layout.bottom_navigation_layout,this,true)
+
+    }
 }
